5aa20f9956444757cf1d18f926e2268eaf1a7517
Ali <ali@knodemy.com>
2025-08-29 17:17:54 -0400

    Dev branch merge with main

--- app.py
+++ app.py
@@ -72,7 +72,7 @@ origins = [
 app.add_middleware(
     CORSMiddleware,
     allow_origins=origins,
-    #allow_origin_regex=r"^https://([a-z0-9-]+\.)?knodemy\.ai$",  #keeping this for production
+    allow_origin_regex=r"^https://([a-z0-9-]+\.)?knodemy\.ai$",  #keeping this for production
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
